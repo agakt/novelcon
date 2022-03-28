ba8815b368a8bab025b11ac1ea02e198d99d6592
woody.lee <ldm1235@naver.com>
2022-03-28 22:37:50 +0900

    Spring initializer

---
+++ src/main/java/com/example/novelcon/NovelconApplication.java
@@ -0,0 +1,13 @@
+package com.example.novelcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelconApplication.class, args);
+	}
+
+}
